943f4b642d1f6f96f469d0d9402f3613d6216d4c
seyonaru <vlarina0001@gmail.com>
2025-02-18 02:17:28 +0700

    lab 3 task 1

--- ProgrammingPractice/ProgrammingPractice.cpp
+++ ProgrammingPractice/ProgrammingPractice.cpp
@@ -1,6 +1,8 @@
 ﻿#include <iostream>
 #include <math.h>
 #include <vector>
+#include <stdlib.h>
+#include <conio.h>
 
 using namespace std;
 
@@ -13,25 +15,24 @@ void Easter(int year) {
     int e = (2 * b + 4 * c + 6 * d + 6) % 7;
     int f = d + e;
 
-    if (f <= 26) cout << "Easter will be at "<< f + 4 << "'th of April";
-    else cout << "Easter will be at "<<f - 26 << "'th of May";
+    if (f <= 26) cout << "Easter will be at " << f + 4 << "'th of April";
+    else cout << "Easter will be at " << f - 26 << "'th of May";
 
     //Масленица
     if ((f - 57 >= -56) && (f - 57 <= -31)) cout << "\n Масленица будет " << f + 8 << " февраля";
     else cout << "\n Масленица будет " << f - 20 << " марта";
 }
-
 void power(double x, int n) {
     double y = x;
     if (n > 0) {
-        for (int i = 0; i < n-1; i++) {
+        for (int i = 0; i < n - 1; i++) {
             y *= x;
         }
     }
     else {
         n *= -1;
         double temp = x;
-        for (int i = 0; i < n-1; i++) {
+        for (int i = 0; i < n - 1; i++) {
             temp *= x;
         }
         y = 1 / temp;
@@ -39,7 +40,6 @@ void power(double x, int n) {
 
     cout << y;
 }
-
 void lab1() {
     //for task 1 
     int year = 2025;
@@ -79,7 +79,6 @@ void RecursionSorting(vector<int>& Array, int index = 0, bool isNegative = true)
     }
     RecursionSorting(Array, index + 1, isNegative);
 }
-
 void TransitionToBinary(int N, std::vector<int>& ans) {
     if (N == 0) {
         if (ans.empty()) {
@@ -99,12 +98,11 @@ void TransitionToBinary(int N, std::vector<int>& ans) {
         cout << ans[i];
     }
 }
-
 void lab2() {
     cout << "Task 1: ";
     Recursion();
 
-    vector<int> Array = {2,3,7,-11, 6, -5, 6, -1, 4};
+    vector<int> Array = { 2,3,7,-11, 6, -5, 6, -1, 4 };
     const int N = 0;
     bool isNegative = true;
     cout << "\nTask 2: ";
@@ -115,26 +113,84 @@ void lab2() {
     cout << "\nTask 3: ";
     TransitionToBinary(A, ans);
 }
+
+//lab3
+int usingMalloc() {
+    int i;
+    int n;
+    int* b;
+    int* c; //positive
+    int* d; //negative
+
+    cout << "Size of massive -> ";
+    cin >> n;
+    cout << "\n";
+
+    b = (int*)malloc(n * sizeof(int));
+
+    if (b == NULL) {
+        cout << "Error in resizing storage";
+        return 1;
+    }
+
+    for (i = 0; i < n; i++) {
+        b[i] = rand() % 1000 / 1000. + rand() % 2000 - 1000;
+    }
+
+    int cntN = 0; //negative count
+    int cntP = 0; //positive count
+
+    for (i = 0; i < n; i++) {
+        if (b[i] < 0) cntN++;
+        else cntP++;
+    }
+
+    c = (int*)malloc(cntP * sizeof(int));
+    d = (int*)malloc(cntN * sizeof(int));
+    int tempN = 0;
+    int tempP = 0;
+    for (i = 0; i < n; i++) {
+        if (b[i] < 0) {
+            d[tempN] = b[i];
+            tempN++;
+        }
+        else {
+            c[tempP] = b[i];
+            tempP++;
+        }
+    }
+
+    cout << "main massive: [ ";
+    for (i = 0; i < n; i++) {
+        cout << b[i] << " ";
+    }
+    cout << "], Length of massive: "<< n << endl;
+
+    cout << "massive with positive numbers: [ ";
+    for (i = 0; i < cntP; i++) {
+        cout << c[i] << " ";
+    }
+    cout << "], Length of massive: " << cntP << endl;
+
+    cout << "massive with negative numbers: [ ";
+    for (i = 0; i < cntN; i++) {
+        cout << d[i] << " ";
+    }
+    cout << "], Length of massive: " << cntN << endl;
+
+    free(b);
+    free(c);
+    free(d);
+    return 0;
+}
+
+void lab3() {
+    usingMalloc();
+}
 int main()
 {
-    /*
-    //for task 1 
-    int year = 2021;
-    
-    //for task 2
-    int n1 = 9;
-    int n2 = -4;
-    double x = 1.2;
-
-    //results
-    cout << "Task 1: ";
-    Easter(year);
-    cout << "\nTask 2: Incoming numbers:\n Variant 1: n = " << n1 <<", x =" << x << " result: ";
-    power(x, n1);
-    cout << "\n Variant 1: n = " << n2 << ", x =" << x << " result: ";
-    power(x, n2);
-    */
-
+    srand(0);
     //lab1();
-    lab2();
+    //lab2();
+    lab3();
 }
